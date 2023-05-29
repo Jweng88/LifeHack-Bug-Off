1b1c4932b4f39cbba8ca8d02f9119a0ab577f22b
jayllo-c <jaylotc@gmail.com>
2023-05-30 02:19:21 +0800

    attempted to fix bug pertaining the command setDoc()

--- bug-off/src/components/Signup.js
+++ bug-off/src/components/Signup.js
@@ -8,7 +8,7 @@ import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import SensorOccupiedIcon from '@mui/icons-material/SensorOccupied';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -29,8 +29,10 @@ const SignUp = ({ setIsSignUp }) => {
   const [accExists, setAccExists] = useState(false);
   const [shortPw, setShortPw] = useState(false);
 
+  
   const onSubmit = async (e) => {
     e.preventDefault();
+    console.log(email, password);
     await createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
@@ -62,66 +64,69 @@ const SignUp = ({ setIsSignUp }) => {
 
   // format taken from https://github.com/mui/material-ui/blob/v5.13.2/docs/data/material/getting-started/templates/sign-in/SignIn.js
   return (
-    <main >    
-        <ThemeProvider theme={theme}>
-          <Container component="main" maxWidth="xs">
-            <CssBaseline />
-              
-          </Container>
-        </ThemeProvider>    
-        <section>
-            <div>
-                <div>                  
-                    <h1> FocusApp </h1>                                                                            
-                    <form>                                                                                            
-                        <div>
-                            <label htmlFor="email-address">
-                                Email address
-                            </label>
-                            <input
-                                type="email"
-                                label="Email address"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}  
-                                required                                    
-                                placeholder="Email address"                                
-                            />
-                        </div>
-
-                        <div>
-                            <label htmlFor="password">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                label="Create password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)} 
-                                required                                 
-                                placeholder="Password"              
-                            />
-                        </div>                                             
-                        
-                        <button
-                            type="submit" 
-                            onClick={onSubmit}                        
-                        >  
-                            Sign up                                
-                        </button>
-                                                                     
-                    </form>
-                    {accExists && 
-                      <h2>email already in use</h2>
-                    }
-                    {shortPw && 
-                      <h2>password must be at least 6 characters long</h2>
-                    }
-                                   
-                </div>
-            </div>
-        </section>
-    </main>
-  )
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 10,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: '#29a2ed'}}>
+              <SensorOccupiedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign Up
+            </Typography>
+            <Box component="form" onSubmit={onSubmit} noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ 
+                  mt: 3, 
+                  mb: 2,
+                  fontWeight: 700
+                }}
+              >
+                Sign In
+              </Button>
+            </Box>
+            {accExists && 
+              <h2>email already in use</h2>
+            }
+            {shortPw && 
+              <h2>password must be at least 6 characters long</h2>
+            }
+          </Box>
+        </Container>
+      </ThemeProvider>     
+    );
 }
 
 export default SignUp;
