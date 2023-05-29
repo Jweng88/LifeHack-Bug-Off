9b2b44b080c25b5dd4affb60a881e55823c28313
tamago55 <pughalraj77@gmail.com>
2023-05-29 13:58:14 +0800

    just made it into a login component

--- bug-off/src/App.js
+++ bug-off/src/App.js
@@ -1,87 +1,12 @@
 import { useEffect, useState } from 'react'
-import useFirebaseConfig from "./useFirebaseConfig"
+import Login from "./components/Login"
 import './App.css';
 
-
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-} from "firebase/auth";
-
-
 function App() {
-  const  { app, analytics } = useFirebaseConfig(); 
-
-  const auth = getAuth();
-  const [data, setData] = useState({
-    email: "",
-    password: "",
-  });
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleInputs = (event) => {
-    const inputs = { [event.target.name]: event.target.value };
-    setData({ ...data, ...inputs });
-  };
-
-  const addData = () => {
-    signInWithEmailAndPassword(auth, data.email, data.password)
-      .then(() => {
-        setIsLoggedIn(true);
-      })
-      .catch((error) => {
-        console.log("Login error:", error);
-      });
-      console.log(`${data.email} ${data.password}`)
-  };
-
-  const handleLogout = () => {
-    signOut(auth)
-      .then(() => {
-        setIsLoggedIn(false);
-      })
-      .catch((error) => {
-        console.log("Logout error:", error);
-      });
-  };
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
-      }
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, [auth]);
-
-
   return (
     <div className ="App-header">
-      <div className="input-fields"> 
-        <input
-            placeholder="Email"
-            name="email"
-            type="email"
-            onChange={event => handleInputs(event)}
-        />
-        <input
-          placeholder="Password"
-          name="password"
-          type="password"
-          className="input-fields"
-          onChange={event => handleInputs(event)}
-        />
-      </div>
-  <button onClick={addData}>Log In</button>
-  <button onClick={handleLogout}>Log out</button>
-</div>
+      <Login />
+    </div>
   );
 }
 
---
+++ bug-off/src/components/Login.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react'
+import useFirebaseConfig from "../useFirebaseConfig"
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+  } from "firebase/auth";
+
+export default function Login() {
+    const  { app, analytics } = useFirebaseConfig(); 
+
+    const auth = getAuth();
+    const [data, setData] = useState({
+      email: "",
+      password: "",
+    });
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+    const handleInputs = (event) => {
+      const inputs = { [event.target.name]: event.target.value };
+      setData({ ...data, ...inputs });
+    };
+  
+    const addData = () => {
+      signInWithEmailAndPassword(auth, data.email, data.password)
+        .then(() => {
+          setIsLoggedIn(true);
+        })
+        .catch((error) => {
+          console.log("Login error:", error);
+        });
+        console.log(`${data.email} ${data.password}`)
+    };
+  
+    const handleLogout = () => {
+      signOut(auth)
+        .then(() => {
+          setIsLoggedIn(false);
+        })
+        .catch((error) => {
+          console.log("Logout error:", error);
+        });
+    };
+  
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      });
+  
+      return () => {
+        unsubscribe();
+      };
+    }, [auth]);
+  
+  
+    return (
+    <div className ="LoginForm">
+        <div className="input-fields"> 
+          <input
+              placeholder="Email"
+              name="email"
+              type="email"
+              onChange={event => handleInputs(event)}
+          />
+          <input
+            placeholder="Password"
+            name="password"
+            type="password"
+            className="input-fields"
+            onChange={event => handleInputs(event)}
+          />
+        </div>
+        <button onClick={addData}>Log In</button>
+        <button onClick={handleLogout}>Log out</button>
+    </div>
+    );
+}
