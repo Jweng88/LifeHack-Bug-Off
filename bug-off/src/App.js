24d73f09521a722cdc9bfc9e78bad13aa14e1362
tamago55 <pughalraj77@gmail.com>
2023-05-29 14:03:03 +0800

    conditional rendering for login page

--- bug-off/src/App.js
+++ bug-off/src/App.js
@@ -1,11 +1,12 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Login from "./components/Login"
 import './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className ="App-header">
-      <Login />
+      { !isLoggedIn && <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}
     </div>
   );
 }
